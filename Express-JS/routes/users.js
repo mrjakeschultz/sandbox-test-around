6dd7efae8ea43e46d59976ec33dfb40e1b9a8542
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2022-11-28 18:02:02 -0600

    Added to express js routes

---
+++ Express-JS/routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	res.send("User List");
+});
+
+router.get("/new", (req, res) => {
+	res.send("Users New Form");
+});
+
+router.post("/", (req, res) => {
+	res.send("Post Route to make a new user");
+});
+
+router.put("/:id", (req, res) => {
+	res.send(`Get User With ID ${req.params.id}`);
+});
+
+router.get("/:id", (req, res) => {
+	res.send(`Get User With ID ${req.params.id}`);
+});
+
+module.exports = router;
--- Express-JS/server.js
+++ Express-JS/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
 	console.log(
 		"🚀 ~ file: server.js ~ line 7 ~ app.get ~ testing square one get route",
 		app.get
 	);
-	res.render("index");
+	res.render("index", {
+		text: "Look how the value from the 'text' object is injected/called here on the html page being rendered.",
+	});
 });
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 
 app.listen(3000);
